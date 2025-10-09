ada95d37b653e6d1c28a6318364060d728c53355
BeoGonzalez <beo.gonzalez@duocuc.cl>
2025-10-09 16:42:46 -0300

    Una tarjetita lista

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 import Home from './components/Home';
+import Card from './components/Card';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <Home />
+    <Home />,
+    <Card />
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+function Card() {
+return (
+  <div className="card w-25 mb-3" style={{width: '18rem', }}>
+    <img src="https://casaroyal.vtexassets.com/arquivos/ids/163607-800-800?v=638874964810800000&width=800&height=800&aspect=true" className="card-img-center" alt="mouse gamer" />
+    <div className="card-body">
+      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+    </div>
+  </div>
+  );
+}
+
+export default Card;
