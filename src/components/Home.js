ea5c0a9e6cb04a26aa3104c7cfa0e164a06fcec5
W610-PC18 <labinf@PMAWSW6LC0918.duoc.acad>
2025-10-09 16:14:16 -0300

    Se aplicó el tema oscuro a la página web

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import bootstrap from 'bootstrap'
+
+function Home() {
+  return (
+    <div className="container-fluid mt-5 px-5">
+      <h1 className="text-primary display-4 fw-bold ">
+        Bienvenido a GamerShop
+      </h1>
+      <p className="lead">
+        Bievenido a la tienda GamerShop en donde encontrarás juegos y accesorios para todo gamer.
+      </p>
+      
+    </div>
+  )
+}
+
+export default Home
