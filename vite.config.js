78643a3ef996af1737d98e6ac1b920a8a41bb08a
BeoGonzalez <beo.gonzalez@duocuc.cl>
2025-11-03 00:31:43 -0300

    se cambio el pckage.json y el vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   // depending on your application, base can also be "/"
-  base: "",
+  base: "/",
   plugins: [react(), viteTsconfigPaths()],
   server: {
     // this ensures that the browser opens upon server start
